f25899e91ea1d65c83985ac1819217be8f1fb87f
roberto <roberto@interdata.com.ar>
2015-11-13 20:09:48 -0300

    ok

---
+++ myTest8/forms/formInicio.js
@@ -0,0 +1,11 @@
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @properties={typeid:24,uuid:"108070CB-7D53-4C65-93FB-22E4E79C8C85"}
+ */
+function onActionVer(event) {
+	application.showForm(forms.formInicioDetalle)
+}
